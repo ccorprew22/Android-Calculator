d25f3424c0557c5a312c56c952385eece70fae70
ccorprew <ccorprew22@hotmail.com>
2019-03-22 23:45:48 -0400

    Task: Changed adding and subtracting process, breaks after first equation Task: Added variables

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Broken
+    //Somewhat works at launch for addition and subtraction
     private Button addBtn;
     private Button subBtn;
     private Button mulBtn;
@@ -28,7 +28,12 @@ public class MainActivity extends AppCompatActivity {
     private Button equalBtn;
     private Button clearBtn;
     private TextView resultTextView;
-    static int result = 0;
+    //static int result = 0;
+    static String symbol = "";
+    static Integer val1 = 0;
+    static Integer val2 = 0;
+    static String num = "";
+    static Integer res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupUIViews();
-        //String val = "";
-        //String val2 = "";
+
 
 
         zeroBtn.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 if(resultTextView.getText().toString() == "0") {
                     resultTextView.setText(resultTextView.getText().toString());
                 }else{
-                    resultTextView.setText("resultTextView.getText().toString()" + "0");
+                    resultTextView.setText(resultTextView.getText().toString() + "0");
                 }
             }
         });
@@ -53,9 +57,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 if(resultTextView.getText().toString() == "0"){
                     resultTextView.setText("1");
-                }else {
+                }else{
                     resultTextView.setText(resultTextView.getText().toString() + "1");
                 }
+                num += "1";
             }
         });
         twoBtn.setOnClickListener(new View.OnClickListener() {
@@ -65,51 +70,57 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     resultTextView.setText(resultTextView.getText().toString() + "2");
                 }
+                num += "2";
             }
         });
         threeBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                    if(resultTextView.getText().toString() == "0"){
-                        resultTextView.setText("3");
-                    }else {
-                        resultTextView.setText(resultTextView.getText().toString() + "3");
-                    }
+                if(resultTextView.getText().toString() == "0"){
+                    resultTextView.setText("3");
+                }else {
+                    resultTextView.setText(resultTextView.getText().toString() + "3");
+                }
+                num += "3";
             }
         });
         fourBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                    if(resultTextView.getText().toString() == "0"){
-                        resultTextView.setText("4");
-                    }else {
-                        resultTextView.setText(resultTextView.getText().toString() + "4");
-                    }
+                if(resultTextView.getText().toString() == "0"){
+                    resultTextView.setText("4");
+                }else {
+                    resultTextView.setText(resultTextView.getText().toString() + "4");
+                }
+                num += "4";
             }
         });
         fiveBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                    if(resultTextView.getText().toString() == "0"){
-                        resultTextView.setText("5");
-                    }else {
-                        resultTextView.setText(resultTextView.getText().toString() + "5");
-                    }
+                if(resultTextView.getText().toString() == "0"){
+                    resultTextView.setText("5");
+                }else {
+                    resultTextView.setText(resultTextView.getText().toString() + "5");
+                }
+                num += "5";
             }
         });
         sixBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                    if(resultTextView.getText().toString() == "0"){
-                        resultTextView.setText("6");
-                    }else {
-                        resultTextView.setText(resultTextView.getText().toString() + "6");
-                    }
+                if(resultTextView.getText().toString() == "0"){
+                    resultTextView.setText("6");
+                }else {
+                    resultTextView.setText(resultTextView.getText().toString() + "6");
+                }
+                num += "6";
             }
         });
         sevenBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                    if(resultTextView.getText().toString() == "0"){
-                        resultTextView.setText("7");
-                    }else {
-                        resultTextView.setText(resultTextView.getText().toString() + "7");
-                    }
+                if(resultTextView.getText().toString() == "0"){
+                    resultTextView.setText("7");
+                }else {
+                    resultTextView.setText(resultTextView.getText().toString() + "7");
+                }
+                num += "7";
             }
         });
         eightBtn.setOnClickListener(new View.OnClickListener() {
@@ -119,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
                 }else {
                     resultTextView.setText(resultTextView.getText().toString() + "8");
                 }
+                num += "8";
             }
         });
         nineBtn.setOnClickListener(new View.OnClickListener() {
@@ -128,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
                 }else {
                     resultTextView.setText(resultTextView.getText().toString() + "9");
                 }
+                num += "9";
             }
         });
 
@@ -136,20 +149,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                int val = Integer.parseInt(resultTextView.getText().toString());
-
-                result = result + val;
-                resultTextView.setText("");
+                if (symbol == "") {
+                    val1 = Integer.parseInt(num);
+                    symbol = "+";
+                    num = "";
+                }else if(symbol != ""){
+                    val2 = Integer.parseInt(num);
+                    val1 = compute(val1, symbol, val2);
+                    num = "";
+                }
+                resultTextView.setText(resultTextView.getText().toString() + " + ");
             }
         });
         subBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                int val = Integer.parseInt(resultTextView.getText().toString());
-
-                result = result - val;
-                resultTextView.setText("");
+                if (symbol == "") {
+                    val1 = Integer.parseInt(num);
+                    symbol = "-";
+                    num = "";
+                }else if(symbol != ""){
+                    val2 = Integer.parseInt(num);
+                    val1 = compute(val1, symbol, val2);
+                    num = "";
+                }
+                resultTextView.setText(resultTextView.getText().toString() + " - ");
             }
         });
         mulBtn.setOnClickListener(new View.OnClickListener() {
@@ -157,34 +182,40 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 int val = Integer.parseInt(resultTextView.getText().toString());
+                String symbol = "*";
 
-                result = result * val;
-                resultTextView.setText("");
+                Integer res = compute(val1, symbol, val2);
+                resultTextView.setText("0");
             }
         });
         divBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                int val = Integer.parseInt(resultTextView.getText().toString());
+                int val1 = Integer.parseInt(resultTextView.getText().toString());
+                String symbol = "/";
 
-                result = result / val;
-                resultTextView.setText("");
+                res = compute(val1, symbol, val2);
+                resultTextView.setText("0");
             }
         });
         equalBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-                resultTextView.setText(Integer.toString(result));
+                val2 = Integer.parseInt(num);
+                val1 = compute(val1, symbol, val2);
+                symbol = "";
+                resultTextView.setText(val1.toString());
             }
         });
         clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                result = 0;
+                val1 = 0;
+                val2 = 0;
+                symbol = "";
                 resultTextView.setText("0");
             }
         });
@@ -211,6 +242,20 @@ public class MainActivity extends AppCompatActivity {
         equalBtn = (Button) findViewById(R.id.equalBtn);
         clearBtn = (Button) findViewById(R.id.clearBtn);
         resultTextView = (TextView) findViewById(R.id.resultTextView);
+    }
+
+    public static int compute(int v1, String sym, int v2){
+        int result = 0;
+        if(sym == "+"){
+            result = v1 + v2;
+        }else if(sym == "-"){
+            result = v1 - v2;
+        }else if(sym == "/"){
+            result = v1 / v2;
+        }else if(sym == "*"){
+            result = v1 * v2;
+        }
+        return result;
     }
 
 }
